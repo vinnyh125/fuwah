cc586ea4d46e55e69a7d5a2e2935d4f165b4dc4b
Vincent Huang <vh222@cornell.edu>
2023-06-05 05:46:45 -0400

    Created dropdown box
    +added dropdown box with shadows
    +centered some elements
    +added jquery code to hide and show dropdown

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
+    var toggle = 0;
+
     $('.navbar a').on('click', function() {
         $('.navbar a').not(this).removeClass('underline');
         $(this).toggleClass('underline');
-      });
+        if (!$(this).hasClass('dropbtn') && toggle == 1) {
+            $('.dropdown-content').hide();
+            toggle = 0;
+        }
+    });
+    
+    $('.dropbtn').click(function() {
+        if (toggle == 0) {
+            toggle = 1;
+            $('.dropdown-content').show();
+        }
+        else {
+            $('.dropdown-content').hide();
+            toggle = 0;
+        }
+    });
 });

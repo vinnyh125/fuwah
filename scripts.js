67163cc9500bf32a05d22bbdc803295ba6db557c
Vincent Huang <vh222@cornell.edu>
2023-06-10 02:12:35 -0400

    Added a list of popular items
    +added time tracking and updates site to reflect whether or not we're open at a certain hour on a certain day
    +added popular items
    +Vegetarian options
    +added appetizers
    +added prices for all aforementioned items
    +added header for Full Menu
    -removed text that talked about location and interactive google map for time being (might be too distracting/look too out of place)
    
    Plans:
    >Work on creating slideshow for showcase back and front of menu
    >Work on javascript animation for different parts of the site
    >Work on styling/revising bottom info bar
    >Work on styling site over all with some colors
    >Work on integrating different fonts

--- scripts.js
+++ scripts.js
@@ -30,8 +30,84 @@ $(document).ready(function() {
         "September", "October", 
         "November", "December"
     ];
+    const dayOfWeekList = [
+        "Monday", "Tuesday",
+        "Wednesday", "Thursday",
+        "Friday", "Saturday", 
+        "Sunday"
+    ]
+
     y = n.getFullYear();
     m = monthList[n.getMonth()];
     d = n.getDate();
+    dayOfWeek = dayOfWeekList[n.getDay()];
+    hourOfDay = n.getHours();
+    minuteOfDay = n.getMinutes();
+    restOpen = document.getElementById('hour-minute').innerHTML = "We are currently: OPEN"
+    restClosed = document.getElementById('hour-minute').innerHTML = "We are currently: CLOSED"
+
     document.getElementById('date').innerHTML = m.toUpperCase() + " " + d + ", " + y;
+    if (dayOfWeek in dayOfWeekList.splice(4,3)) {
+        if (hourOfDay < 22) {
+            if (hourOfDay >= 10) {
+                if (hourOfDay == 10 && minuteOfDay >= 30) {
+                    restOpen;
+                }
+                else if (hourOfDay == 10 && minuteOfDay < 30) {
+                    restClosed;
+                }
+                else {
+                    restOpen;
+                } 
+            }
+            else {
+                restOpen;
+            }
+        }
+        else {
+            restClosed;
+        }
+    }
+    else if (dayOfWeek == "Friday" || dayOfWeek == "Saturday") {
+        if (hourOfDay < 23) {
+            if (hourOfDay >= 10) {
+                if (hourOfDay == 10 && minuteOfDay >= 30) {
+                    restOpen;
+                }
+                else if (hourOfDay == 10 && minuteOfDay < 30) {
+                    restClosed;
+                }
+                else {
+                    restOpen;
+                } 
+            } 
+            else {
+                restOpen;
+            } 
+        }
+        else {
+            restClosed;
+        }
+    }
+    else {
+        if (hourOfDay < 22) {
+            if (hourOfDay >= 11) {
+                if (hourOfDay == 11 && minuteOfDay >= 30) {
+                    restOpen;
+                }
+                else if (hourOfDay == 11 && minuteOfDay < 30) {
+                    restClosed;
+                }
+                else {
+                    restOpen;
+                }
+            }
+            else {
+                restOpen;
+            }
+        }
+        else {
+            restClosed;
+        }
+    }
 });
